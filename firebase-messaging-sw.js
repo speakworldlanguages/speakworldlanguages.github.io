edb278886dc22f6048b97e689d917beaab72285d
TopraksoyEarthmanTsuchimoto <0000u0571u1985+github@gmail.com>
2021-11-06 12:23:11 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,26 +9,26 @@
 // import { getMessaging } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-messaging.js";
 // import { onBackgroundMessage } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-messaging.js";
 //
-const brokenApiKey = "IzaSyDYBQrC1GFMYtsWtR8tOTanfE09I4alX50";
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "A"+brokenApiKey,
-  authDomain: "using-firebase-service.firebaseapp.com",
-  databaseURL: "https://using-firebase-service-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "using-firebase-service",
-  storageBucket: "using-firebase-service.appspot.com",
-  messagingSenderId: "624023469269",
-  appId: "1:624023469269:web:2116d2ffa20b3394f1765c",
-  measurementId: "G-C23BKN622X"
-};
+// const brokenApiKey = "IzaSyDYBQrC1GFMYtsWtR8tOTanfE09I4alX50";
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "A"+brokenApiKey,
+//   authDomain: "using-firebase-service.firebaseapp.com",
+//   databaseURL: "https://using-firebase-service-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "using-firebase-service",
+//   storageBucket: "using-firebase-service.appspot.com",
+//   messagingSenderId: "624023469269",
+//   appId: "1:624023469269:web:2116d2ffa20b3394f1765c",
+//   measurementId: "G-C23BKN622X"
+// };
 //
 // // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
 // const analytics = firebase.analytics();
 // const messaging = firebase.messaging();
 
-const firebaseApp = initializeApp(firebaseConfig);
-const messaging = getMessaging(firebaseApp);
+// const firebaseApp = initializeApp(firebaseConfig);
+// const messaging = getMessaging(firebaseApp);
 
 onBackgroundMessage(messaging, (payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
