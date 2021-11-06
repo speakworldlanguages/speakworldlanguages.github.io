cd23bfeb1bf35fb8e0a377bef63badbb095fbed5
TopraksoyEarthmanTsuchimoto <0000u0571u1985+github@gmail.com>
2021-11-06 08:50:51 +0200

    test notification

--- db_en.js
+++ db_en.js
@@ -28,17 +28,3 @@ function insertData() {
   set(ref(db,"en/"+index.toUTCString()),{"Token":tokenToBeSaved}).then(()=>{console.log("token is saved");}).catch((error)=>{console.log("couldn't save token: "+error);});
 }
 const brokenVapidKey = "B7_p1Mfhfo4YbGkmKRDjemU0tPEGcZ3zzysITjcrPMzjR3x38cKyRmzG1T7ID3YdXC-QqSRgxLntBmAJ8tkn04";
-function subscribeUser() {
-  Notification.requestPermission.then(permission=>{
-    if (permission == "granted") {
-      getToken(messaging, {vapidKey:"B"+brokenVapidKey}).then((currentToken) => {
-        tokenToBeSaved = currentToken;
-        insertData();
-      }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-      });
-    }
-  });
-}
-const clickToSubscrise = document.getElementById('footerNotificationID');
-clickToSubscrise.addEventListener("click",subscribeUser); // Do we need once:true???
