4884fdcd24216bc71df3b90a32acc33fcb3cc85a
TopraksoyEarthmanTsuchimoto <0000u0571u1985+github@gmail.com>
2023-09-04 02:30:53 +0300

    Update js_for_different_browsers_and_devices.js

--- js_reusables/js_for_different_browsers_and_devices.js
+++ js_reusables/js_for_different_browsers_and_devices.js
@@ -86,8 +86,7 @@ window.addEventListener('DOMContentLoaded', function(){
     // We don't want to put this into js_for_every_single_html because that precedes all js files
   } else {
     // Set the sound file format for the entire app
-    soundFileFormat = "mp3";
-    //soundFileFormat = "webm"; // CAUTION: Parent level only! See js_for_all_iframed_lesson_htmls to find how it's passed to the iframe level
+    soundFileFormat = "webm"; // CAUTION: Parent level only! See js_for_all_iframed_lesson_htmls to find how it's passed to the iframe level
     // We don't want to put this into js_for_every_single_html because that precedes all js files
   }
   // -
